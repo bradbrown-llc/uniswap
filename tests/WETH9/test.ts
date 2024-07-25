35ae54e63f3682d0f10e0184e097ea350e7ec628
Brad Brown <bradbrown@bradbrown.llc>
2024-07-25 00:04:44 +0000

    actual test, not just log

--- tests/WETH9/test.ts
+++ tests/WETH9/test.ts
@@ -1,3 +1,7 @@
+import { assert } from "https://deno.land/std@0.224.0/assert/assert.ts";
 import { WETH9 } from "../../WETH9.ts";
 
-console.log(WETH9)
+Deno.test('WETH9 object and bytecode test', () => {
+    assert(WETH9 && WETH9.bytecode)
+    console.log(WETH9.bytecode)
+})
