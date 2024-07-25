f83639a3eaf6c880988fd624c5d19ef755521ac1
Brad Brown <bradbrown@bradbrown.llc>
2024-07-25 00:04:53 +0000

    use remote container

--- WETH9.ts
+++ WETH9.ts
@@ -1,7 +1,7 @@
 import { Docker } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/Docker@0.0.2/mod.ts'
 import * as Solc from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/Solc@0.0.7/mod.ts'
 
-const rawCompilationResults = await Docker.exec('weth9')
+const rawCompilationResults = await Docker.exec('ghcr.io/bradbrown-llc/weth9:1.0')
 const compilationResults = Solc.schemas.solcCompilationOutput.parse(JSON.parse(rawCompilationResults))
 const bytecode = compilationResults.contracts!['WETH9.sol']!['WETH9']!.evm!.bytecode!.object!
 export const WETH9 = { bytecode }
