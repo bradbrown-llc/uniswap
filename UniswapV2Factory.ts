70e2a39f1252f53a05168ff43680cd73d08e6558
Brad Brown <bradbrown@bradbrown.llc>
2024-07-25 04:59:43 +0000

    uniswapv2factory compilation result image interface thing

---
+++ UniswapV2Factory.ts
@@ -0,0 +1,12 @@
+import { hexToBytes } from 'npm:@noble/hashes@1.3.3/utils'
+import jsSha3 from 'npm:js-sha3@0.9.2'
+const { keccak256 } = jsSha3
+import { Docker } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/Docker@0.0.2/mod.ts'
+import * as Solc from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/Solc@0.0.7/mod.ts'
+
+const rawCompilationResults = await Docker.exec('bradthomasbrown/uniswapv2factory:1.0')
+const compilationResults = Solc.schemas.solcCompilationOutput.parse(JSON.parse(rawCompilationResults))
+const bytecode = compilationResults.contracts!['UniswapV2Factory.sol']!['UniswapV2Pair']!.evm!.bytecode!.object!
+const pairInitCode = compilationResults.contracts!['UniswapV2Factory.sol']!['UniswapV2Pair']!.evm!.bytecode!.object!
+const pairInitCodeHash = keccak256(hexToBytes(pairInitCode))
+export const UniswapV2Factory = { bytecode, pairInitCodeHash }
