76c38c29aa237a2daddc8395848875547206ad13
Brad Brown <bradbrown@bradbrown.llc>
2024-07-25 20:42:30 +0000

    add new interfaces to barrel

--- mod.ts
+++ mod.ts
@@ -1,3 +1,5 @@
 // if you import from this mod.ts, this will have the side effect of downloading the images for all of the below
 export * from './UniswapV2Factory.ts'
+export * from './UniswapV3Factory.ts'
+export * from './UniversalRouter.ts'
 export * from './WETH9.ts'
