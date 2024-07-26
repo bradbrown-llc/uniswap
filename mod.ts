d7517a57cd1e62dce3f14cc63af2f481e41033d4
Brad Brown <bradbrown@bradbrown.llc>
2024-07-26 16:01:44 +0000

    add NFTDescriptor comp res int to barrel

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 // if you import from this mod.ts, this will have the side effect of downloading the images for all of the below
+export * from './NFTDescriptor.ts'
 export * from './NonfungiblePositionManager.ts'
 export * from './NonfungibleTokenPositionDescriptor.ts'
 export * from './UniswapV2Factory.ts'
