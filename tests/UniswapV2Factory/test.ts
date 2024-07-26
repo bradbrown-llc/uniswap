9a5dacd290bfa6244e67a83a22ec6f7f8693007c
Brad Brown <bradbrown@bradbrown.llc>
2024-07-26 14:48:21 +0000

    update test name

--- tests/UniswapV2Factory/test.ts
+++ tests/UniswapV2Factory/test.ts
@@ -1,7 +1,7 @@
 import { assert } from "https://deno.land/std@0.224.0/assert/assert.ts";
 import { UniswapV2Factory } from "../../UniswapV2Factory.ts";
 
-Deno.test('WETH9 object and properties test', () => {
+Deno.test('UniswapV2Factory object and properties test', () => {
     assert(
         UniswapV2Factory
         && UniswapV2Factory.bytecode
