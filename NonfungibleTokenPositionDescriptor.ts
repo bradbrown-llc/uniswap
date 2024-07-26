4b310070642c6149895c808122d909d5f11779c8
Brad Brown <bradbrown@bradbrown.llc>
2024-07-26 16:08:00 +0000

    NonfungibleTokenPositionDescriptor bytecode is now a function that takes NFTDescriptor address and links the library that way

--- NonfungibleTokenPositionDescriptor.ts
+++ NonfungibleTokenPositionDescriptor.ts
@@ -2,7 +2,8 @@ import { Docker } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/Docker@0.0.2/m
 import * as Solc from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/solc@0.0.8/mod.ts'
 
 const rawCompilationResults = await Docker.exec('bradthomasbrown/nonfungibletokenpositiondescriptor:1.0')
-console.log(rawCompilationResults)
 const compilationResults = Solc.schemas.solcCompilationOutput.parse(JSON.parse(rawCompilationResults))
 const bytecode = compilationResults.contracts!['NonfungibleTokenPositionDescriptor.sol']!['NonfungibleTokenPositionDescriptor']!.evm!.bytecode!.object!
-export const NonfungibleTokenPositionDescriptor = { bytecode }
+export const NonfungibleTokenPositionDescriptor = {
+    bytecode: (nftDescriptorAddress:string) => bytecode.replace(/__\$88153e50d1a910aee6c4ab4932e1a7f343\$__/, nftDescriptorAddress.slice(2))
+}
--- tests/NonfungibleTokenPositionDescriptor/test.ts
+++ tests/NonfungibleTokenPositionDescriptor/test.ts
@@ -2,6 +2,6 @@ import { assert } from "https://deno.land/std@0.224.0/assert/assert.ts";
 import { NonfungibleTokenPositionDescriptor } from "../../NonfungibleTokenPositionDescriptor.ts";
 
 Deno.test('NonfungibleTokenPositionDescriptor object and properties test', () => {
-    assert(NonfungibleTokenPositionDescriptor && NonfungibleTokenPositionDescriptor.bytecode)
-    console.log(NonfungibleTokenPositionDescriptor)
+    assert(NonfungibleTokenPositionDescriptor && NonfungibleTokenPositionDescriptor.bytecode(''.padEnd(40, 'A')))
+    console.log(NonfungibleTokenPositionDescriptor.bytecode(''.padEnd(40, 'A')))
 })
